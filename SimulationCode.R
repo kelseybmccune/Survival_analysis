e2da812e73134ee6ba885f015c24e8b941190d92
Jonathon J. Valente <Jonathon.J.Valente@gmail.com>
2024-04-20 11:51:46 -0500

    With chi-squared goodness of fit test

--- SimulationCode.R
+++ SimulationCode.R
@@ -330,5 +330,80 @@ summary(em1)
 
 
 
+#Now we are going to evaluate the fit of this fitted model. To do that, we're going to use
+#the fitted model to simulate a bunch of datasets. This gives us a sense of what kinds
+#of data this fitted model generates. We're then going to evaluate whether the kinds of
+#data this model generates look like the data we actually collected.
+
+#Simulate 500 datasets from the fitted model
+nSims = 500
+tmp1 = simulate(em1, nsim=nSims)
+
+#We are going to calculate a chi-squared value for each simulated dataset, so we
+#are creating a vector in which to save those chi-squared values.
+simResults = rep(NA, nSims)
+
+
+for(i in 1:nSims){
+  
+  #The "observed" values in each simulated dataset are based on the explanatory variables
+  #in the original dataset. So the easiest way to create a full new simulated dataset
+  #is just to pull the original data, and then replace the true observed data with the
+  #simulated observed data
+  tmp2 = exp %>%
+    mutate(LatencyFirstLand = tmp1[,i])
+  
+  #So now that we have a simulated dataset, we have our observed values. To get our
+  #expected values, we fit the exact same model again (this time using the simulated
+  #response variables).
+  tmp3 = glmer(round(LatencyFirstLand) ~ Condition + FlexGroup + (1|BirdID),
+               data = tmp2, family = "poisson")
+  
+  #So we have our "observed" values which are simulated
+  o = tmp2$LatencyFirstLand
+  
+  #And our "expected" values which are generated from the model fit to the
+  #simulated observed values
+  e = predict(tmp3, type='response')
+  
+  #And we can caluclate a chi-squared value for this dataset.
+  simResults[i] = sum((o-e)^2/e, na.rm=T)
+}
+
+#When we do this 500 times, we get a distribution of the kinds of chi-squared values that
+#this fitted model would generate IF it is in fact the correct representation of the data
+
+
+
+#Now let's calculate a chi-squred value for the original dataset from the original
+#fitted model
+modChisq = sum((exp$LatencyFirstLand - predict(em1, type='response'))^2/predict(em1, type='response'))
+
+
+#Lastly, we can plot the distribution of chi-squared values from the simulations and
+#compare this distribution to the one we actually observed. If our original fitted model
+#is a good representation of our original fitted data, then the original chi-squared value
+#should be similar to those generated from our simulations
+tmp = data.frame(simResults)
+
+ggplot(tmp, aes(x=simResults))+
+  geom_histogram(fill='white', color='black', binwidth=5)+
+  theme_bw()+
+  theme(panel.grid=element_blank())+
+  geom_vline(xintercept=modChisq, color='red')
+
+#And we can calculate a 'p-value' of sorts by asking what proportion of the simulated
+#chi-squared values are larger or smaller than chi-squared value observed from the
+#original data and model. Here, we want our p-value to be around 0.5 indicating that
+#about half of the chi-square values from the simulations are larger than the observed
+#value and the other half are smaller. If the observed value falls in the upper or lower
+#2.5% of simulated values, we would say there is significant evidence (at the alpha level
+#of 0.05) that the model doesn't fit our original dataset very well.
+
+
+sum(simResults > modChisq)/length(simResults)
+
+
+
 
 
